2ab7f1085e3e9c617f9970a9cdd3e5ed6bf5c8be
ChrisM-Pek <99971150+ChrisM-Pek@users.noreply.github.com>
2025-03-18 01:45:02 +0100

    open url in browser not in app

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, shell } = require('electron');
 const path = require('path');
 
 function createWindow() {
@@ -6,12 +6,31 @@ function createWindow() {
         width: 1280,
         height: 720,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            contextIsolation: false // Assurez-vous que cela est désactivé si vous utilisez nodeIntegration
         },
         icon: path.join(__dirname, 'images', 'icon.png')
     });
 
     win.loadURL('https://chat.mistral.ai');
+
+    // Intercepter les tentatives de navigation dans la même fenêtre
+    win.webContents.on('will-navigate', (event, url) => {
+        event.preventDefault();
+        shell.openExternal(url);
+    });
+
+    // Intercepter les tentatives d'ouverture de nouvelles fenêtres
+    win.webContents.setWindowOpenHandler(({ url }) => {
+        shell.openExternal(url);
+        return { action: 'deny' };
+    });
+
+    // Intercepter les clics sur les liens qui ouvrent de nouvelles fenêtres
+    win.webContents.on('new-window', (event, url) => {
+        event.preventDefault();
+        shell.openExternal(url);
+    });
 }
 
 app.whenReady().then(createWindow);
